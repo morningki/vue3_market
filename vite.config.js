a320467527c4eb1299d030f95d852a97bd2f54b3
morningki <1274396405@qq.com>
2026-03-28 00:38:24 +0800

    feat:element自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    // 自动导入Element Plus组件，不用手动import
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
+    //实际路径转换
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },

4db302f6914332f77730ce36d0bacd97c476d3a0
morningki <1274396405@qq.com>
2026-03-30 10:53:08 +0800

    '整体结构拆分和分类数据渲染'

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,18 @@
-<template>
-  <div></div>
-</template>
-
-<script setup lang="ts">
-
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
 </script>
 
-<style scoped>
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
 
-</style>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
+</template>
